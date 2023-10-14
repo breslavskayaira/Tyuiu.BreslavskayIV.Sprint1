96f9b67e64bb2ca06c1c8c9fb7082417978165e0
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-10-14 23:29:28 +0500

    Add files via upload

---
+++ Tyuiu.BreslavskyaIV.Sprint1.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BreslavskyaIV.Sprint1.Task2.V12.Lib;
+
+namespace Tyuiu.BreslavskyaIV.Sprint1.Task2.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            int y = 1;
+            int z = 1;
+            var res = ds.CalculateParallelepipedVolume(x, y, z);
+            Assert.AreEqual(1, res);
+            
+        }
+    }
+}
---
+++ Tyuiu.BreslavskyaIV.Sprint1.Task2.V12/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BreslavskyaIV.Sprint1.Task2.V12.Lib;
+
+namespace Tyuiu.BreslavskyaIV.Sprint1.Task2.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Бреславская И. В. | ПКТб-23-2";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                      *");
+            Console.WriteLine("* Задание #2                                                               *");
+            Console.WriteLine("* Вариант #12                                                              *");
+            Console.WriteLine("* Выполнила: Бреславская И. В. | ПКТб-23-2                                 *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя длины сторон      *");
+            Console.WriteLine("* параллелепипеда, находит его объем и печатает результат на экране        *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            int x, y, z;
+            Console.WriteLine("Введите значение Х: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Z: ");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Объем параллелепипеда равен: " + ds.CalculateParallelepipedVolume(x, y, z));
+            Console.ReadLine();
+        }
+    }
+}
