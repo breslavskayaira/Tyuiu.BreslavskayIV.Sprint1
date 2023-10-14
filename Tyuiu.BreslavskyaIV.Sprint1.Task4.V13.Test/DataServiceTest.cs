661616818349c901675bd98150f8d3dd1ff4d5e1
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-10-14 23:33:31 +0500

    Add files via upload

---
+++ Tyuiu.BreslavskyaIV.Sprint1.Task4.V13.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BreslavskyaIV.Sprint1.Task4.V13.Lib;
+
+namespace Tyuiu.BreslavskyaIV.Sprint1.Task4.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 0.0;
+          
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(-0.123, res);
+        }
+    }
+}
---
+++ Tyuiu.BreslavskyaIV.Sprint1.Task4.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BreslavskyaIV.Sprint1.Task4.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BreslavskyaIV.Sprint1.Task4.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("498ef98a-4db8-492a-b102-1e9c391ba45d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
